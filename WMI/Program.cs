ffe9b2ba125f6b141560122267f8c311b7b3454e
rakdav <rakdav@inbox.ru>
2026-01-30 14:35:00 +0200

    WMI-2

--- WMI/Program.cs
+++ WMI/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WMI.Samples;
+
+BiosReader reader = new BiosReader();
+reader.ReadBiosDetails();
---
+++ WMI/Samples/TemperatureReader.cs
@@ -0,0 +1,32 @@
+﻿using ExtensionLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMI.Samples
+{
+    public class TemperatureReader
+    {
+        public void TemperatureReaderUsingWMI() {
+            var scope = "root\\WMI";
+            var query = "SELECT * FROM MSAcpi_ThermalZoneTemperature";
+            var searcher=new ManagementObjectSearcher(scope, query);
+            try
+            {
+                foreach (var item in searcher.Get())
+                {
+                    var obj = (ManagementObject)item;
+                    var temperature = Convert.ToDouble(obj["CurrentTemperature"])/10-273.15;
+                    $"CPU Temperature:{temperature}".Dump();
+                }
+            }
+            catch(ManagementException)
+            {
+                "BIOS не поддерживает API".Dump(ConsoleColor.Red);
+            }
+        }
+    }
+}
