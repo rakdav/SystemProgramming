800900862a014624897fa624b8be4e4d304916fd
rakdav <rakdav@inbox.ru>
2026-01-30 14:22:59 +0200

    WMI

---
+++ WMI/Samples/BiosReader.cs
@@ -0,0 +1,30 @@
+﻿using ExtensionLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMI.Samples
+{
+    public class BiosReader
+    { 
+        public void ReadBiosDetails() {
+            ManagementScope scope=new ManagementScope("\\\\.\\ROOT\\cimv2");
+            scope.Connect();
+            ObjectQuery query=new ObjectQuery("SELECT * FROM Win32_BIOS");
+            using ManagementObjectSearcher searcher=new ManagementObjectSearcher(scope,query);
+            foreach (var o in searcher.Get())
+            {
+                var queryObj = (ManagementObject)o;
+                "______________________".Dump(ConsoleColor.Yellow);
+                "BIOS Infornmation".Dump(ConsoleColor.Yellow);
+                "______________________".Dump(ConsoleColor.Yellow);
+                $"Manufactorer:{queryObj["Manufacturer"]}".Dump(ConsoleColor.Yellow);
+                $"Name:{queryObj["Name"]}".Dump(ConsoleColor.Yellow);
+                $"Version:{queryObj["Version"]}".Dump(ConsoleColor.Yellow);
+            }
+        }
+    }
+}
