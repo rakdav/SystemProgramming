de03ae79730c1de328b0a9c3d52060e2635057fe
Andrews <rakdav@inbox.ru>
2026-03-18 11:21:57 +0200

    last

--- Arduino/MainWindow.xaml.cs
+++ Arduino/MainWindow.xaml.cs
@@ -22,13 +22,14 @@ namespace Arduino
         public MainWindow()
         {
             InitializeComponent();
-            
-
         }
         private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            string receivedData = serialPort.ReadLine();
-            Answer.Text += "Received from Arduino:" + receivedData;
+            Dispatcher.Invoke(() =>
+            {
+                string receivedData = serialPort.ReadLine();
+                Answer.Text += "Received from Arduino" + receivedData;
+            });
         }
 
         private void SendToArduino(string data)
@@ -39,18 +40,34 @@ namespace Arduino
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-                serialPort = new SerialPort(comPort.SelectedValue.ToString(), 9600);
+            serialPort = new SerialPort(comPort.SelectedValue.ToString(), 9600);
+
             serialPort.DataReceived += SerialPort_DataReceived;
             try
             {
                 serialPort.Open();
                 serialPort.Encoding = Encoding.UTF8;
-                SendToArduino(Message.Text);
             }
             catch (Exception ex)
             {
 
             }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SendToArduino(Message.Text);
+            }
+            catch (Exception ex) 
+            {
+
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            serialPort?.Close();
         }
     }
 }
