6064541a42254a45115551e406e5c9f61423685a
rakdav <rakdav@inbox.ru>
2026-04-30 12:24:48 +0200

    signal

---
+++ FirstSignalR/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace FirstSignalR
+{
+    public class ChatHub:Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await this.Clients.All.SendAsync("Receive",message,userName);
+        }
+    }
+}
---
+++ FirstSignalR/Program.cs
@@ -0,0 +1,9 @@
+using FirstSignalR;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.MapHub<ChatHub>("/chat");
+app.Run();
